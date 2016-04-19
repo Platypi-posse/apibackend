a81546c0d585056704e87c7fc703198acc3a9bff
Josh English <josh@joshenglish.com>
2016-04-19 09:56:13 -0500

    Inital setup of API Backend for models, db and removal of Jade

---
+++ models/schema.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+// locationSchema
+
+var locationSchema = new mongoose.Schema({
+  location_id: Number,
+  coordinates: String,
+  art_piece_name: String,
+  artist: [],
+  description: String,
+  directions: String
+});
+
+var Location = mongoose.model('Location', locationSchema);
+
+// imageSchema
+
+var imageSchema = new mongoose.Schema({
+  username: String,
+  location_id: String,
+  date: Date,
+  image: String,
+  isApproved: Boolean
+});
+
+var Image = mongoose.model('Image', imageSchema);
+
+module.exports = {
+  Location: Location,
+  Image: Image
+};
