6bb680fb5205f42e2c416e8b491ce0f369c3321d
Josh English <josh@joshenglish.com>
2016-04-19 09:59:37 -0500

    Changin routes to controllers folder and in app.js

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var controllers = require('./controllers/index');
+var users = require('./controllers/users');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', controllers);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
